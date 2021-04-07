bee2b26d29f59a38b54fd6d2d35b34968271856a
Megarekrut65 <bandalak28@gmail.com>
2021-04-07 15:48:57 +0300

    Added a function to count the length of a number

--- algebra_lab/large_number_pow.cpp
+++ algebra_lab/large_number_pow.cpp
@@ -1,6 +1,30 @@
 #include "large_number_pow.h"
 
-Large_Number pow(const Large_Number& number, const Large_Number& exp)
+namespace ln
 {
+	Large_Number pow(const Large_Number& number, const Large_Number& exp)
+	{
 
+	}
+	unsigned pow(const unsigned& number, const unsigned& exp)
+	{
+		if (exp == 0) return 1;
+		if (exp == 1) return number;
+		unsigned base = 10;
+		unsigned modulus = 101;
+		unsigned numberR = std::pow(base, length(modulus, base));
+		numberR %= modulus;
+
+		return number;
+	}
+	unsigned length(unsigned number, unsigned base)
+	{
+		unsigned res = 0;
+		while (number > 0)
+		{
+			number /= base;
+			res++;
+		}
+		return res;
+	}
 }
--- algebra_lab/large_number_pow.h
+++ algebra_lab/large_number_pow.h
@@ -1,4 +1,11 @@
 #pragma once
 #include "Large_Number.h"
+#include <math.h>
+#include <vector>
+namespace ln//large number
+{
+	Large_Number pow(const Large_Number& number, const Large_Number& exp);
+	unsigned pow(const unsigned& number, const unsigned& exp);
+	unsigned length(unsigned number, unsigned base);
 
-Large_Number pow(const Large_Number& number, const Large_Number& exp);
+}
