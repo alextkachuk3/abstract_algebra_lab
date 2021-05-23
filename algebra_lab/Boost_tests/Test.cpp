69dcecaf7497e5134b277158ff2f3e634a65e8ae
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2021-05-23 21:09:23 +0300

    Added minus test

--- algebra_lab/Boost_tests/Test.cpp
+++ algebra_lab/Boost_tests/Test.cpp
@@ -27,15 +27,33 @@ struct LN {
 
 BOOST_FIXTURE_TEST_CASE(Sum, LN)
 {
-firstNum->value.push_back((unsigned int)(0));
-secondNum->value.push_back((unsigned int)(30));
-Large_Number result = *firstNum + *secondNum;
-cout << result.to_string();
-BOOST_CHECK_EQUAL(result.to_string(), "10000001e");
+    firstNum->value.push_back((unsigned int)(0));
+    secondNum->value.push_back((unsigned int)(30));
+    Large_Number result = *firstNum + *secondNum;
+    cout << result.to_string();
+    BOOST_CHECK_EQUAL(result.to_string(), "10000001e");
 
-firstNum->value.push_back((unsigned int)(0));
-secondNum->value.push_back((unsigned int)(-30));
-result = *firstNum + *secondNum;
-BOOST_CHECK_EQUAL(result.to_string(), "10000001fffffffe2");
+    firstNum->value.push_back((unsigned int)(0));
+    secondNum->value.push_back((unsigned int)(-30));
+    result = *firstNum + *secondNum;
+    BOOST_CHECK_EQUAL(result.to_string(), "10000001fffffffe2");
 
+}
+
+BOOST_FIXTURE_TEST_CASE(Minus, LN)
+{
+    firstNum->value.push_back((unsigned int)(22));
+    secondNum->value.push_back((unsigned int)(12));
+    Large_Number result = *firstNum - *secondNum;
+    BOOST_CHECK_EQUAL(result.to_string(), "a");
+
+    firstNum->value.push_back((unsigned int)(60));
+    secondNum->value.push_back((unsigned int)(123456));
+    result = *firstNum - *secondNum;
+    BOOST_CHECK_EQUAL(result.to_string(), "9fffe1dfc");
+
+    firstNum->value.push_back((unsigned int)(-60));
+    secondNum->value.push_back((unsigned int)(123456));
+    result = *firstNum - *secondNum;
+    BOOST_CHECK_EQUAL(result.to_string(), "9fffe1dfcfffe1d84");
 }
