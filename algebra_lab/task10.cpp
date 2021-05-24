a1d41c5c2ea874679133fef00be934603bca2c52
P34R <54835953+P34R@users.noreply.github.com>
2021-05-24 12:16:48 +0300

    changed ints to LN

--- algebra_lab/task10.cpp
+++ algebra_lab/task10.cpp
@@ -1,13 +1,14 @@
 #include "task10.h"
 Large_Number power(Large_Number& x, Large_Number& y, Large_Number& p)
 {
+    Large_Number two(2);
     Large_Number res(1);
     x = x % p;
     while (y > 0)
     {
         if (y.value[y.value.size()-1]& 1)
             res = (res*x) % p;
-        y = y/2;
+        y = y/two;
         x = (x*x) % p;
     }
     return res;
@@ -18,21 +19,21 @@ bool miillerTest(Large_Number& d, Large_Number& n)
     if (nm.value[nm.value.size()-1]>4) nm.value[nm.value.size()-1]=nm.value[nm.value.size()-1]-3;
     Large_Number a;
     if (nm.value.size()>1)
-        a=rand();
+        a.push_back(rand());
     else if (nm.value.size()==1) {
-        a = rand() % nm.value[0];
+        a.push_back(rand() % nm.value[0]);
         a++;
     }
-    else a.value.push_back(0);
+    else return 0;
     nm.value[nm.value.size()-1]=nm.value[nm.value.size()-1]+3;
     Large_Number x=power(a,d,n);
-    if ((x.value.size()==1 && x.value[0] == 1 )  || x == nm)
+    if ((x.to_string()=="1")  || x == nm)
         return true;
     while (!(d == nm))
     {
         x = (x * x) % n;
         d = d.multiply_by_digit(2);
-        if (x.value.size()==1 && x.value[0] == 1 )      return false;
+        if (x.to_string()=="1" )      return false;
         if (x == nm)    return true;
     }
     return false;
