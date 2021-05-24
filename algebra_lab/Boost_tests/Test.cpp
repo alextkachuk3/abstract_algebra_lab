a826fbcb4a1a34b77ad11c9ac89d6388d69a61b5
alextkachuk3 <55971220+alextkachuk3@users.noreply.github.com>
2021-05-24 10:56:14 +0300

    remove large_num test from cmake

--- algebra_lab/Boost_tests/Test.cpp
+++ algebra_lab/Boost_tests/Test.cpp
@@ -5,7 +5,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 
-#include <boost/test/unit_test.hpp>
+//#include <boost/test/unit_test.hpp>
 #include "../Large_Number/Large_Number.h"
 
 struct LN {
