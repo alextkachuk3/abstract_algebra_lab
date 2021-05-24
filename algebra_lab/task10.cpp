6eee63e1d1067dd1b9aa9b51d2ce237dcce94784
P34R <P34R@yandex.ru>
2021-05-24 11:45:58 +0300

    task10 own pow

--- algebra_lab/task10.cpp
+++ algebra_lab/task10.cpp
@@ -1,5 +1,17 @@
 #include "task10.h"
-#include "pow/large_number_pow.h"/*
+Large_Number power(Large_Number& x, Large_Number& y, Large_Number& p)
+{
+    Large_Number res(1);
+    x = x % p;
+    while (y > 0)
+    {
+        if (y.value[y.value.size()-1]& 1)
+            res = (res*x) % p;
+        y = y/2;
+        x = (x*x) % p;
+    }
+    return res;
+}
 bool miillerTest(Large_Number& d, Large_Number& n)
 {
     Large_Number nm=n; nm.value[nm.value.size()-1]-=1;
@@ -7,11 +19,13 @@ bool miillerTest(Large_Number& d, Large_Number& n)
     Large_Number a;
     if (nm.value.size()>1)
         a=rand();
-    else if (nm.value.size()==1)
-        a=rand()%nm.value[0]+1;
-    else a=0;
+    else if (nm.value.size()==1) {
+        a = rand() % nm.value[0];
+        a++;
+    }
+    else a.value.push_back(0);
     nm.value[nm.value.size()-1]=nm.value[nm.value.size()-1]+3;
-    Large_Number x=ln::powNumber(a,d)%n;
+    Large_Number x=power(a,d,n);
     if ((x.value.size()==1 && x.value[0] == 1 )  || x == nm)
         return true;
     while (!(d == nm))
@@ -25,13 +39,14 @@ bool miillerTest(Large_Number& d, Large_Number& n)
 }
 bool isPrime(Large_Number& n, int k)
 {
+    Large_Number two(2);
     Large_Number d = n; d.value[d.value.size()-1]--;
-    while (d.value[d.value.size()-1] % 2 == 0) {
-        d = d / 2;
+    while (d % two == 0) {
+        d = d / two;
     }
     for (int i = 0; i < k; i++)
         if (!miillerTest(d,n))
             return false;
 
     return true;
-}*/
+}
